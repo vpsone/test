e240d6c914b3cc98cb43d6678f731f464dabdd28
vpsone <84894255+vpsone@users.noreply.github.com>
2023-09-14 23:09:14 +0530

    Update generate-pull-requests.js

--- generate-pull-requests.js
+++ generate-pull-requests.js
@@ -1,41 +1,19 @@
-const axios = require('axios');
 const fs = require('fs');
+const token = process.env.GH_TOKEN;
+const url = 'https://api.github.com/graphql';
 
-async function fetchPullRequests() {
-  const token = process.env.GH_TOKEN;
-  const headers = {
-    Authorization: `token ${token}`,
-  };
-
-  const response = await axios.get(
-    'https://api.github.com/graphql',
-    {
-      headers,
-      data: {
-        query: `
-          {
-            viewer{
-              pullRequests(last: 15){
-                nodes {
-                  title
-                  url
-                }
-              }
-            }
-          }
-        `,
-      },
-    }
-  );
-
-  const pullRequests = response.data.data.viewer.pullRequests.nodes;
-  return pullRequests;
-}
-
-async function generatePullRequestsMarkdown() {
-  const pullRequests = await fetchPullRequests();
-  const markdown = pullRequests.map((pr) => `- [${pr.title}](${pr.url})`).join('\n');
-  fs.writeFileSync('pull-requests.md', markdown);
-}
-
-generatePullRequestsMarkdown();
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Authorization': `bearer ${token}`,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: '  {    "query": "query { viewer { pullRequests(last: 50){ nodes { title url }} }}"  } '
+})
+  .then(response => response.json())
+  .then(data => {
+    const pullRequest = data.data.viewer.pullRequests.nodes;
+    const markdown = pullRequest.map((pr) => `- [${pr.title}](${pr.url})`).join('\n');
+    fs.writeFileSync('pull-requests.md', markdown);
+  })
+  .catch(error => console.error('Error:', error));
